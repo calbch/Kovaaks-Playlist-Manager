83ac04e1c9166ddab3467611004e028b1a39e2c0
DondiBronson <coluckenbach@adct.com>
2020-08-21 12:39:07 +0200

    json storage

---
+++ KovaaksPlaylistManager/UserSettingManager.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace KovaaksPlaylistManager
+{
+    public class UserSettingManager
+    {
+        public static UserSettings LoadUserSettings()
+        {
+            var userSettingsJson = File.ReadAllText(@"userSettings.json");
+            return JsonSerializer.Deserialize<UserSettings>(userSettingsJson);
+        }
+
+        public static bool SaveUserSettings(UserSettings settings)
+        {
+            File.WriteAllText(@"userSettings.json", JsonSerializer.Serialize(settings));
+            return LoadUserSettings().Equals(settings);
+        }
+    }
+}
---
+++ KovaaksPlaylistManager/UserSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KovaaksPlaylistManager
+{
+    public class UserSettings
+    {
+        public string PlaylistPath { get; set; }
+        
+        
+        public override bool Equals(Object obj)
+        {
+            //Check for null and compare run-time types.
+            if ((obj == null) || ! this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                return this.PlaylistPath.Equals(((UserSettings) obj).PlaylistPath);
+            }
+        }
+    }
+}
